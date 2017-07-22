d6df80b8bc35655f83071abb772cfa0733cebc8d
gmlrnjs1994 <gmlrnjs1994@naver.com>
2017-07-22 00:31:44 -0400

    fetch

--- a.s
+++ a.s
@@ -16,30 +16,29 @@ _start:
 		;push rbp
 		;mov rbp, rsp
 		;sub rsp, 0x420	;	buffer[1024]
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
 	mov rax, 0x29
 	mov rdi, 2	; AF_INET
 	mov rsi, 1	; socket stream
 	mov rdx, 0	
 	syscall		; socket open
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
 		;mov r8, rax	; r8 -> client_socket
 		;mov [rbp-0x4], rax
 		;mov rax, 2
 		;mov [rbp-0x20], rax
 	
 		;mov rdi, rax
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	mov rdi, rax	; fd	
 	mov rsi, addr	; 0x7fffffffe0c0
 	mov rdx, 0x10
 	mov rax, 42
 	syscall		; connet open
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 		;mov [rbp-0x8], rax
 		;lea rcx, [rbp-0x420]	; rcx <- buffer[1024]
-		;mov rax, [rbp-0x4]	; rax <- client_socket
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		;mov rax, [rbp-0x4]	; rax <- client_socke
+
 	sub rsp, 0x40	; 64 byte area is maked
 	mov rdx, 0x3f	; 0x400
 	mov rsi, rsp	; rcx
@@ -50,7 +49,7 @@ _start:
 	mov r10, 0
 	mov rax, 45
 	syscall		; read open
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 	mov r8, 0	
 	mov r9, rax	; r8 <- rax <- len
 	mov rax, 1
@@ -93,30 +92,28 @@ parse:		; parse data
 	add r8, 1
 
 	mov al, [rsi + r8]
-	;shl rax, 8
+		;shl rax, 8
 	add r8, 1
 	
 	push rax
 	loop parse
 complete:
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	;cmp r8, r9
-	;jl parse
+		;cmp r8, r9
+		;jl parse
 	
 	mov r14, r8
 	sub r14, r9
 	mov r15, r14
 	;mov r15, r9
 
-;	mov rdi, 1
-;	;mov rsi, rsp
-;	add r14, rsp
-;	mov rsi, r14
-;	mov rdx, r9
-;	mov rax, 1
-;	syscall		; print reverse data
-	;;;;;;;;;;;;;;
+	;	mov rdi, 1
+	;	;mov rsi, rsp
+	;	add r14, rsp
+	;	mov rsi, r14
+	;	mov rdx, r9
+	;	mov rax, 1
+	;	syscall		; print reverse data
 
 	mov rax, 44
 	mov rsi, rsp
